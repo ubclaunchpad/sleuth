603fd61d04a21783b808d3ac0dfa908b2e3ae14d
Bruno Bachmann <bfbachmann@gmail.com>
2017-09-27 22:07:04 -0700

    Add frontend build to docker-compose, add CORS support to Django

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM python:3
 # Set current working directory in the container
 WORKDIR /home/sleuth
 # Copy requirements into the docker container
-COPY requirements.txt ./
+COPY requirements.txt .
 # Install required Python packages
 RUN pip install -r requirements.txt
 # Copy the contents of the current working directory to the container
--- sleuth_backend/sleuth_backend/settings.py
+++ sleuth_backend/sleuth_backend/settings.py
@@ -39,9 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.admindocs',
     'haystack',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -91,6 +93,10 @@ HAYSTACK_CONNECTIONS = {
         'URL': 'http://127.0.0.1:8983/solr',
     },
 }
+
+CORS_ORIGIN_WHITELIST = (
+    'localhost:8080',
+)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
