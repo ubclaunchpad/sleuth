2e2f53f5c311bd7289b1a2b0d220d1782ca5a94b
Bruno Bachmann <bfbachmann@gmail.com>
2017-09-28 22:28:30 -0700

    Add travis config, move sleuth_backend folder up

--- sleuth_backend/settings.py
+++ sleuth_backend/settings.py
@@ -77,15 +77,27 @@ WSGI_APPLICATION = 'sleuth_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'HOST': 'db',
-        'PORT': 5432,
+if 'TRAVIS' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE':   'django.db.backends.postgresql_psycopg2',
+            'NAME':     'testdb',
+            'USER':     'postgres',
+            'PASSWORD': '',
+            'HOST':     'localhost',
+            'PORT':     5432,
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME':   'postgres',
+            'USER':   'postgres',
+            'HOST':   'db',
+            'PORT':   5432,
+        }
+    }
 
 HAYSTACK_CONNECTIONS = {
     'default': {
