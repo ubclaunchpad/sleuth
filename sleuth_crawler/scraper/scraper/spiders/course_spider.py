5db4de0cbdbae562763424eea5ef3f515e8f7a57
bobheadxi <robertlin1@gmail.com>
2017-09-29 17:02:47 -0700

    Switch logging to use Scrapy pipeline

--- sleuth_crawler/scraper/scraper/spiders/course_spider.py
+++ sleuth_crawler/scraper/scraper/spiders/course_spider.py
@@ -1,7 +1,8 @@
 import scrapy
 
 class CourseSpider(scrapy.Spider):
-    """Spider that crawls courses.students.ubc.ca
+    """
+    Spider that crawls courses.students.ubc.ca
     For all detailed course data.
     """
 
@@ -15,25 +16,15 @@ class CourseSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        child_urls = []
-        column0 = []
-        column1 = []
-        column2 = []
-
+        output = []
         rows = response.xpath('//tbody/tr')
         for row in rows:
-            child_urls.append(row.xpath('./td/a/@href').extract_first())
-            column0.append(row.xpath('./td/a/text()').extract_first())
-            column1.append(row.xpath('./td[2]/text()').extract_first())
+            data = {}
+            data['url'] = row.xpath('./td/a/@href').extract_first()
+            data['col1'] = row.xpath('./td/a/text()').extract_first()
+            data['col2'] = row.xpath('./td[2]/text()').extract_first()
             # doesn't always have 2nd column in child links
-            column2.append(row.xpath('./td[3]/text()').extract_first())
-
-        self.__write_to_log("child-links", child_urls)
-        self.__write_to_log("column0", column0)
-        self.__write_to_log("column1", column1)
-        self.__write_to_log("column2", column2)
-
-    def __write_to_log(self, filename, items):
-        with open(filename + ".txt", "wb") as page:
-            for item in items:
-                page.write(str(item) + "\n")
+            data['col3'] = row.xpath('./td[3]/text()').extract_first()
+            output.append(data)
+        
+        return output
