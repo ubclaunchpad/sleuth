907bca709fc14948c580f18e650befa6a4786059
Robert Lin <robertlin1@gmail.com>
2017-10-07 00:29:05 -0700

    Fix Solr insertion in solr_module, hardcode to core TEST

--- sleuth_backend/solr_module/solr_module.py
+++ sleuth_backend/solr_module/solr_module.py
@@ -1,7 +1,8 @@
 import pysolr
 
 solr_url = 'http://localhost:8983/solr/'
-solr_db = pysolr.Solr(solr_url, timeout=10)
+solr_core = "test"
+solr_db = pysolr.Solr(solr_url + solr_core, timeout=10)
 
 def add_item(item):
     """
