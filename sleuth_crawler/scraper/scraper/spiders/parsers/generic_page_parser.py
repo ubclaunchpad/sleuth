646381b8e89cd6dfb347fa4933b25ea1a94ca6ea
Robert Lin <robertlin1@gmail.com>
2017-10-27 21:23:23 -0700

    Add new site_title case to generic_page_parser, improved response mocker

--- sleuth_crawler/scraper/scraper/spiders/parsers/generic_page_parser.py
+++ sleuth_crawler/scraper/scraper/spiders/parsers/generic_page_parser.py
@@ -1,17 +1,21 @@
 import scrapy
+import re
 from sleuth_crawler.scraper.scraper.spiders.parsers import utils
 from sleuth_crawler.scraper.scraper.items import ScrapyGenericPage
 
-def parse_generic_item(response, children):
+def parse_generic_item(response, children=[]):
     """
     Scrape generic page
     """
     site_title = ""
     title = utils.extract_element(response.xpath("//title/text()"), 0)
-    titles = title.split('|')
-    if len(titles) == 2:
+    titles = re.split(r'\| | - ', title)
+    if len(titles) >= 2:
         title = titles[0].strip()
-        site_title = titles[1].strip()
+        site_titles = []
+        for i in range(max(1, len(titles)-2), len(titles)):
+            site_titles.append(titles[i].strip())
+        site_title = " - ".join(site_titles)
     desc = utils.extract_element(response.xpath("//meta[@name='description']/@content"), 0)
     raw_content = utils.strip_content(response.body)
 
@@ -23,3 +27,4 @@ def parse_generic_item(response, children):
         raw_content=raw_content,
         children=children
     )
+    
--- sleuth_crawler/tests/mocks.py
+++ sleuth_crawler/tests/mocks.py
@@ -3,6 +3,7 @@ from scrapy.http import Request, TextResponse
 def mock_response(file_name=None, url=None):
     """
     Create a fake Scrapy HTTP response
+    file_name can be a relative file path or the desired contents of the mock
     """
 
     if not url:
@@ -10,8 +11,12 @@ def mock_response(file_name=None, url=None):
     request = Request(url=url)
 
     if file_name:
-        file_path = "sleuth_crawler/tests" + file_name
-        file_content = open(file_path, 'r').read()
+        try:
+            file_path = "sleuth_crawler/tests" + file_name
+            file_content = open(file_path, 'r').read()
+        except OSError:
+            # Allow mocker to directly input desired mock content
+            file_content = file_name
     else:
         file_content = ""
 
