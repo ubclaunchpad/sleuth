7f820b4a1a46214d79da474e699c19637425e676
Bruno Bachmann <bfbachmann@gmail.com>
2017-12-02 18:35:38 -0800

    Revert changes to run-crawlers script

--- scripts/run-crawlers.sh
+++ scripts/run-crawlers.sh
@@ -1,14 +1,33 @@
 #!/bin/bash
 # Utility script for running crawlers
 
-re='^[0-9]+$'
-if ! [[ $1 =~ $re ]] ; then
-  echo "Must specify number of seconds to run crawlers for" >&2
-  exit 1
-fi
+usage="
+  Usage: `basename $0` [-h] [-t n]
 
+  where:
+    -h    show help text
+    -t    set how long to run spiders for (in seconds)
+"
+
+while getopts ':ht:' option; do
+  case "$option" in
+    h) echo "$usage"
+       exit
+       ;;
+    t) time=$OPTARG
+       ;;
+    :) printf "  Missing argument for -%s\n" "$OPTARG" >&2
+       echo "$usage" >&2
+       exit 1
+       ;;
+   \?) printf "  Illegal option: -%s\n" "$OPTARG" >&2
+       echo "$usage" >&2
+       exit 1
+       ;;
+  esac
+done
+
+echo 'Spinning up crawlers...'
 cd sleuth_crawler/scraper
-echo "Spinning up crawlers..."
-python crawl.py & read -t $1
-kill $!
-echo "Crawlers ran for $1 seconds"
+python crawl.py & read -t $time ; kill $!
+echo 'Crawler run for ' $time ' seconds'
